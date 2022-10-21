0a028347e03b3c4d3958410b5c6f31d7a19ba26e
mamialex <mamialexboost@gmail.com>
2022-10-21 14:41:22 +0200

    provider.tf modified

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "eu-west-2"
-    access_key = "AKIA3YXER6ABHE7B62S3"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 terraform {
